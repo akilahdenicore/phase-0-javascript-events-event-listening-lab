1537aca464c0169fbd2de283714a58fd3a668da2
Akilah Snowden <akilahdsnowden@gmail.com>
2022-05-02 11:17:54 -0400

    commit tree 5d758f6c2a414783dda6973fe0e42bb9218cc816

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-function addingEventListener() {
+/* binds an event listener in addingEventListener():
+ReferenceError: addingEventListener is not defined 
+expect(input.addEventListener.called).to.be.true
+
+
+addEventListener() method on the element we want to add the listener to, 
+and passing it two arguments:
+the name of the event to listen for, and
+a callback function to "handle" the event
+
+->grab the element that we want to add the event listener to 
+and save a reference to it in variable.
+->next call addEventListener() on the element, we pass 2 arguments:
+name of event 'click' to listen for, and callback function that will be executed when event is heard
+callback will be invoked as soon as the event occurs
+*/
+
+//binds an event listener in addingEventListener
+
+//define addingEventListener
+function addingEventListener(){
+//grab element id to bind to event listener
     const input = document.getElementById('input');
-    input.addEventListener('click', function() {
-        alert('I was clicked!');
-    });
+//attach and plug in event 'click', and pass callback func as argument 
+ input.addEventListener('click', function(){ 
+     alert('click demo'); //action to be carried after click event is listened for 
+});
 }
